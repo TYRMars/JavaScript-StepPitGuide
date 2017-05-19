67d922ec59899a3809efdbe4a87e66b722ee3c08
张佳楠 <zhangjianan1996@qq.com>
2017-05-19 16:07:04 +0800

    JDMenu 优化
    优化菜单选取，利用三角形坐标选取

--- JDMenu/js/base.js
+++ JDMenu/js/base.js
@@ -3,11 +3,30 @@
  */
 $(document).ready(function () {
     var sub = $('#sub'),
+        test = $('#test'),
         timer,
         mouseInSub = false,
         activeRow,
-        activeMenu;
-    
+        activeMenu,
+        mouseTrack = [],
+        moveHanlder
+    ;
+
+
+
+    /*鼠标跟踪*/
+    moveHanlder =function (e) {
+        mouseTrack.push({
+            x:e.pageX,
+            y:e.pageY
+        })
+    }
+
+    if(mouseTrack.length >3 ){
+        mouseTrack.shift();
+    }
+
+    /*鼠标状态*/
     sub.on('mouseenter',function(e){
        mouseInSub = true;
        
@@ -15,12 +34,14 @@ $(document).ready(function () {
        mouseInSub = false;
     });
 
-    $('#test')
-        /**/
+    /*事件开始*/
+    test
+        /*当鼠标放到一级菜单上*/
         .on('mouseenter',function (e) {
-            sub.removeClass('none')
+            sub.removeClass('none');
+            $(document).bind('mousemove',moveHanlder)
         })
-        /**/
+        /*当鼠标离开一级菜单上*/
         .on('mouseleave',function (e) {
             console.log('fire mouse leave');
             sub.addClass('none');
@@ -34,8 +55,10 @@ $(document).ready(function () {
                 activeMenu.addClass('none');
                 activeMenu = null;
             }
+
+            $(document).unbind('mousemove',moveHanlder)
         })
-        /**/
+        /*事件代理，动态增加列表项*/
         .on('mouseenter','li',function (e) {
             if(!activeRow){
                 activeRow = $(e.target).addClass('active');
@@ -48,19 +71,39 @@ $(document).ready(function () {
                 clearTimeout(timer);
             }
 
-            timer = setTimeout(function(){
-                if(mouseInSub = false){
-                    return;
-                }
-                activeRow.removeClass('active');
-                activeMenu.addClass('none');
+            /*坐标计算*/
+            var currMousePos = mouseTrack[mouseTrack.length - 1]
+            var leftCorner = mouseTrack[mouseTrack.length - 2]
+            var delay = needDelay(sub, leftCorner, currMousePos)
 
-                activeRow = $(e.target);
-                activeRow.addClass('none');
-                activeMenu = $('#' + activeRow.data('id'));
-                activeMenu.removeClass('none');
-                timer = null;
-            },300)
+            /*如果在三角形内*/
+            if(delay){
+                timer = setTimeout(function(){
+                    if(mouseInSub){
+                        return;
+                    }
 
+                    activeRow.removeClass('active');
+                    activeMenu.addClass('none');
+
+                    activeRow = $(e.target);
+                    activeRow.addClass('active');
+                    activeMenu = $('#' + activeRow.data('id'));
+                    activeMenu.removeClass('none');
+                    timer = null;
+                },300)
+            }else{
+                var prevActiveRow = activeRow;
+                var prevActiveMenu = activeMenu;
+
+                activeRow = $(e.target)
+                activeMenu = $('#' + activeRow.data('id'))
+
+                prevActiveRow.removeClass('active')
+                prevActiveMenu.addClass('none')
+
+                activeRow.addClass('active')
+                activeMenu.removeClass('none')
+            }
         })
-})
+});
